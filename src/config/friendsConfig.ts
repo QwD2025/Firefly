a60dc5eded1abaaaca3eca3d570218c20f7ec473
QWD2025 <82033033@qq.com>
2026-05-18 15:37:00 +0800

    更新内容

--- src/config/friendsConfig.ts
+++ src/config/friendsConfig.ts
@@ -35,7 +35,7 @@ export const friendsConfig: FriendLink[] = [
 	{
 		title: "Yuki",
 		imgurl:
-			"https://yukilove.top/wp-content/uploads/2025/12/1765634941-%E5%BE%AE%E4%BF%A1%E5%9B%BE%E7%89%87_20251213220826_509_3.jpg",
+			"assets/images/f1.avif",
 		desc: "世界，你好！-Yuki的个人博客",
 		siteurl: "https://yukilove.top/",
 		tags: ["Blog"],
